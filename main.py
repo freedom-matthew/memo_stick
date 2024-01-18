775c79f9d3aa502f88d7bed321444afdd22b9c8b
Matěj Svoboda <svobodamatej97@gmail.com>
2024-01-18 22:15:32 +0100

    made some minor changes to UI, debugged fliping cards

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ to_learn = data.to_dict(orient="records")
 current_card = {}
 
 
-def flip_card():
-    canvas.itemconfig(card_title, text="English")
-    canvas.itemconfig(card_word, text=current_card["English"])
-
-
 def next_card():
     global current_card
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=front_card_image)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=back_card_image)
 
 
 window = Tk()
@@ -27,7 +29,8 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_card_image)
+back_card_image = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=front_card_image)
 card_title = canvas.create_text(400, 100, font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=3, sticky="ew")
